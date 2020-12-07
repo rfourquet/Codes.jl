4ba1ebf65269a9a3ff9f65e50689486140710534
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-12-07 15:04:51 +0100

    add docstring for `check_matrix`

--- src/LinearCode.jl
+++ src/LinearCode.jl
@@ -1,9 +1,17 @@
 """
-    generator_matrix(c::Code)
+    generator_matrix(c::LinearCode)
 
 Return the generator matrix of code `c`.
 """
 function generator_matrix end
+
+"""
+    check_matrix(c::LinearCode)
+
+Return the transposed `H` of the parity check matrix of `c`,
+such that `iszero(generator_matrix(c) * H)`.
+"""
+function check_matrix end
 
 # default fallback
 base_field(c::LinearCode) = c.field
